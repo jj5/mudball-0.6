972f4917350053ac5bfaba7e5c83fe916f78c2e6
John Elliot V <jj5@jj5.net>
2025-03-17 00:26:42 +1100

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'MUDBALL_NAME', 'Mudball Web Framework' );
 define( 'MUDBALL_CODE', 'mudball' );
 
-//define( 'MUDBALL_VERSION', '0.6.1711' );
+//define( 'MUDBALL_VERSION', '0.6.1713' );
 define( 'MUDBALL_VERSION_MAJOR', 0 );
 define( 'MUDBALL_VERSION_MINOR', 6 );
-define( 'MUDBALL_VERSION_PATCH', 1711 );
+define( 'MUDBALL_VERSION_PATCH', 1713 );
 
 define(
   'MUDBALL_SVN_DATE',
-  '$Date: 2025-03-17 00:21:01 +1100 (Mon, 17 Mar 2025) $'
+  '$Date: 2025-03-17 00:26:42 +1100 (Mon, 17 Mar 2025) $'
 );
-define( 'MUDBALL_SVN_REVISION', '$Revision: 265733106 $' );
+define( 'MUDBALL_SVN_REVISION', '$Revision: 43460651 $' );
 define( 'MUDBALL_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'MUDBALL_GIT_DATE', '2025-03-17 00:21:01 +1100 (Mon, 17 Mar 2025)' );
+define( 'MUDBALL_GIT_DATE', '2025-03-17 00:26:42 +1100 (Mon, 17 Mar 2025)' );
